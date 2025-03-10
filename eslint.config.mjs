eb4d3860915adee6f3749da14478f2ba5e6eb3ca
MewKay <miorakanto25z@gmail.com>
2025-03-10 06:46:45 +0300

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+];
