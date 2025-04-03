dd1c12b1872e5acf8eacefa3ce7b6caf65574154
MewKay <miorakanto25z@gmail.com>
2025-04-03 20:35:00 +0300

    Create logged-index view

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -2,9 +2,13 @@ const Message = require("../models/Message.model");
 const asyncHandler = require("express-async-handler");
 
 const indexGet = asyncHandler(async (req, res) => {
-  if (!req.isAuthenticated()) {
-    const messages = await Message.findBy();
-    res.render("index", { title: "My Clubhouse", messages: messages });
+  const { user } = req;
+  const indexTitle = "My Clubhouse";
+  let messages;
+
+  if (!req.isAuthenticated() || !user.membership_status) {
+    messages = await Message.findBy();
+    return res.render("index", { title: indexTitle, messages: messages });
   }
 });
 
