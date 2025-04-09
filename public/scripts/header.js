3046b1c60180646c403062a17e7d788996ec77db
MewKay <miorakanto25z@gmail.com>
2025-04-09 17:14:42 +0300

    Create modal view to header

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,4 +1,5 @@
 const dropdown = document.querySelector(".select-action");
+const modal = document.querySelector("dialog");
 
 const handleDropdownClick = (e) => {
   const clickedElement = e.target;
@@ -15,4 +16,21 @@ const handleDropdownClick = (e) => {
   }
 };
 
+const handleModalToggle = (e) => {
+  const clickedElement = e.target;
+  const isElementModalOpen = clickedElement.closest(".member-toggle.add");
+  const isElementModalClose = clickedElement.closest(
+    'dialog button[type="reset"]',
+  );
+
+  if (isElementModalOpen) {
+    modal.showModal();
+  }
+
+  if (isElementModalClose) {
+    modal.close();
+  }
+};
+
 window.addEventListener("click", handleDropdownClick);
+window.addEventListener("click", handleModalToggle);
