34450fb418453e07791dcf7015b1bc7d14d79881
MewKay <miorakanto25z@gmail.com>
2025-04-09 23:14:16 +0300

    Create admin checking middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,4 +6,14 @@ const isAuth = (req, res, next) => {
   res.status(401).send("Unauthorized action");
 };
 
-module.exports = { isAuth };
+const isAdmin = (req, res, next) => {
+  const { user } = req;
+
+  if (user.isAdmin) {
+    return next();
+  }
+
+  res.status(401).send("Unauthorized action");
+};
+
+module.exports = { isAuth, isAdmin };
