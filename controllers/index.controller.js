3993688c579a89aacb3d0ce091e20e5a123af755
MewKay <miorakanto25z@gmail.com>
2025-04-10 14:53:41 +0300

    Change log out request to POST

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -63,14 +63,17 @@ const postDelete = [
   }),
 ];
 
-const loggingOut = (req, res, next) => {
-  req.logout((error) => {
-    if (error) {
-      return next(error);
-    }
+const loggingOut = [
+  isAuth,
+  (req, res, next) => {
+    req.logout((error) => {
+      if (error) {
+        return next(error);
+      }
 
-    res.redirect("/");
-  });
-};
+      res.redirect("/");
+    });
+  },
+];
 
 module.exports = { indexGet, indexPost, postDelete, loggingOut };
--- routes/index.router.js
+++ routes/index.router.js
@@ -4,6 +4,6 @@ const controller = require("../controllers/index.controller");
 router.get("/", controller.indexGet);
 router.post("/create-post", controller.indexPost);
 router.post("/delete-post/:messageId", controller.postDelete);
-router.get("/log-out", controller.loggingOut);
+router.post("/log-out", controller.loggingOut);
 
 module.exports = router;
